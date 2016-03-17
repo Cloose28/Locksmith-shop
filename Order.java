7f0e1f9e36bff1c7f0d7ede559b897af1779978e
Art <ashumanov@gmail.com>
2016-03-17 21:29:14 +0300

    cancel write variabels

--- Order.java
+++ Order.java
@@ -6,23 +6,44 @@ import java.time.LocalDateTime;
  * Created by artem on 2/27/16.
  */
 public class Order {
-    /** Date and time. */
-    private LocalDateTime orderDate;
+    /** Date and time of order.*/
+    private LocalDateTime orderDate = LocalDateTime.now();
+    /** Date and time calculation.*/
+    private LocalDateTime estimatedDate = LocalDateTime.now();
+
+    /** True if the organization*/
+    boolean isOrganization;
     /** The full name of the customer*/
     private String customerName;
 
-
-//    /** Client's name. */
-//    private String name;
-//    /** Client's surname. */
-//    private String surname;
-//    /** Client's patronymic. */
-//    private String patronymic;
-    /** Cost of order in roubles*/
+    /** The name of the product*/
+    private String productName;
+    /** The cost of order in roubles*/
     private int cost;
     /** Phone customer (example 8980..) */
     private String phoneNumber;
     /** The full name of the master*/
     private String masterName;
+
+    /** True if in action*/
+    private boolean isAction;
+    /** True if is ready*/
+    private boolean isReady;
+    /** True if is exerted*/
+    private boolean isExerted;
+
+    /** True if the make*/
+    private boolean isMake;
+    /** True if the repair*/
+    private boolean isRepair;
+    /** True if the duplicate*/
+    private boolean isDuplicate;
+    /** True if the search for defects*/
+    private boolean isSearchForDefects;
+
+    
+
+
+
 
 }
